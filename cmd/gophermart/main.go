94c0ae526051d479408e1cc1ae17c477f7a3b566
Nikita Pavlov <pavlovnp@outlook.com>
2024-11-27 12:11:49 +0100

    feat: init http server

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/npavlov/go-loyalty-service/internal/config"
+	"github.com/npavlov/go-loyalty-service/internal/logger"
+	"github.com/npavlov/go-loyalty-service/internal/utils"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	log := logger.NewLogger().SetLogLevel(zerolog.DebugLevel).Get()
+
+	err := godotenv.Load("server.env")
+	if err != nil {
+		log.Error().Err(err).Msg("Error loading server.env file")
+	}
+
+	cfg := config.NewConfigBuilder(log).
+		FromEnv().
+		FromFlags().Build()
+
+	log.Info().Interface("config", cfg).Msg("Configuration loaded")
+
+	ctx, cancel := utils.WithSignalCancel(context.Background(), log)
+
+	//nolint:exhaustruct
+	server := &http.Server{
+		Addr:         cfg.Address,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		//Handler:      cRouter.GetRouter(),
+	}
+
+	go func() {
+		// Wait for the context to be done (i.e., signal received)
+		<-ctx.Done()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Error().Err(err).Msg("Error shutting down server")
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Error().Err(err).Msg("Error starting server")
+		cancel()
+	}
+
+	log.Info().Msg("Server shut down")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/npavlov/go-loyalty-service
 
 go 1.23.2
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
